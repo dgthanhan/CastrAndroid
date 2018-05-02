12299dd9d4f7f3146451fab8a028d4e5532246a6
Le Quang Thanh <thanh.le@evolus.vn>
2018-05-02 17:31:17 +0700

    Show toolbar

--- src/vn/evolus/castr/MainActivity.java
+++ src/vn/evolus/castr/MainActivity.java
@@ -2,6 +2,7 @@ package vn.evolus.castr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -21,8 +22,15 @@ public class MainActivity extends VideoBrowserActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setupActionBar();
         this.getWebView().loadUrl("https://play.evolus.vn/castr/movie-websites.html");
+    }
+
+    private void setupActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
     private XWebView getWebView() {
@@ -53,14 +61,11 @@ public class MainActivity extends VideoBrowserActivity {
     public void startCasting(String title, String description, String posterURL, String mediaURL) {
         //TODO: implement this
         Log.d(TAG, "startCasting: " + title + " -> url: " + mediaURL + "poster: " + posterURL);
-
-
-
         Intent intent = new Intent(this, LocalPlayerActivity.class);
         /* public static MediaInfo buildMediaInfo(String title, String studio, String subTitle,
             int duration, String url, String mimeType, String imgUrl, String bigImageUrl,
             List<MediaTrack> tracks)*/
-        MediaInfo item = VideoProvider.buildMediaInfo(title, "", "", Integer.MAX_VALUE, mediaURL, "video/mp4", posterURL, posterURL, new ArrayList<MediaTrack>());
+        MediaInfo item = VideoProvider.buildMediaInfo(title, "", "", 0, mediaURL, "video/mp4", posterURL, posterURL, new ArrayList<MediaTrack>());
         intent.putExtra("media", item);
         intent.putExtra("shouldStart", false);
         startActivity(intent);
